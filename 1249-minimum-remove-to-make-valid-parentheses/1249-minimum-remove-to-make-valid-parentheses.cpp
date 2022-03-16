29ec4c3b43cfdc3f43fe30633b3dab9796dcb268
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-03-16 16:17:31 +0530

    Time: 44 ms (27.04%), Space: 10.1 MB (93.98%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
+        //a stack to store the index and the bracket 
         vector<pair<int,char>> st;
         int count=0;
+        //storing all the invalid brackets and its index
         for(auto it=s.begin();it!=s.end();it++){
-            // auto pres = st.end()-1;
             if(*it=='('){
                 st.push_back(make_pair(count,*it));
             }
@@ -21,13 +22,11 @@ public:
         else if(s.length()==st.size())
             return "";
         else{
-            int count=0;
+            //removing the brackets 
+            count=0;
             for(auto it = st.begin();it!=st.end();it++){
-                // auto pres = s.begin()+(*it).first;
                 s.erase(s.begin()+(*it).first-count);
                 count++;
-                
-                // cout<<(*it).first<<*(s.begin());
             }
             
             
