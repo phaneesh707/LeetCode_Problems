86d1474aea9c8cf8126748b4075ccb2a41464ceb
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-04-09 15:44:40 +0530

    Time: 33 ms (16.01%), Space: 13.9 MB (22.19%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        //map to hold the frquency
         map<int,pair<int,int>> a;
         vector<int> sol;
+        
+        //heap to hold the get k frequenct elem in log(n) time
         priority_queue<pair<int,int>> pq;
+        
+        //counting frequency and adding a pair
         for(int i=0;i<nums.size();i++){
             a[nums[i]]=make_pair(++a[nums[i]].first,nums[i]);
             // cout<<a[nums[i]].first<<" "<<a[nums[i]].second<<"\n";
         }
         
             
-          
+         //consturting pq
         for(auto i=a.begin();i!=a.end();i++){
             pq.push(i->second);
             // cout<<i->first<<i->second.second<<"->"<<i->second.first<<"\n";
