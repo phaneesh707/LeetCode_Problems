59622b6151ed564dad28dfd4c44fdb7a3d4f0242
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-04-11 17:55:17 +0530

    Time: 31 ms (74.64%), Space: 14.9 MB (23.36%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        //size of matrix
         int m = grid.size();
         int n = grid[0].size();
         if(k%(m*n)==0)
             return grid;
         
+        //if size of matrix is equal to k then no change 
         k%=(m*n);
         int count=0;
         vector<int> s;
+        
+        //storing the grid value in vector
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++)
                 s.push_back(grid[i][j]);
         }
         
+        
+        //starting from the shift position ,start inserting into grid 
         count = s.size()-k;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
