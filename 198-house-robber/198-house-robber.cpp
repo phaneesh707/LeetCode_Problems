ea34b5db5ded6e1d90d7c571f3f403ccfb19b873
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-02 15:26:31 +0530

    Time: 3 ms (50.43%), Space: 7.9 MB (25.88%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    
     int rob(vector<int>& nums) {
-        int maxi = 0;
+//         Time - O(N) space - O(N)   N-size of nums
         if(nums.size()==1)
             return nums[0];
         if(nums.size()==2)
             return max(nums[0],nums[1]);
         vector<int> a(nums.size(),0);
         a[0]=nums[0];
-        a[1]=nums[1];
-        for(int i=2;i<nums.size();i++){
-            a[i] = nums[i]+*max_element(a.begin(),a.begin()+i-1);
-        }
+        a[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++)
+            a[i] = max(a[i-2]+nums[i],a[i-1]);
         
-        return *max_element(a.begin(),a.end());
-    }
+        
+        for(int i=0;i<nums.size();i++)
+            cout<<a[i]<<" ";
+
+        
+        return a[nums.size()-1];
+    }      
 };
