733f59f4048b6c15aa4edaff57e070aa9d1d4e7c
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-02 15:30:04 +0530

    Time: 5 ms (20.04%), Space: 7.7 MB (47.82%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,21 +3,33 @@ public:
     
     int rob(vector<int>& nums) {
 //         Time - O(N) space - O(N)   N-size of nums
+        // if(nums.size()==1)
+        //     return nums[0];
+        // if(nums.size()==2)
+        //     return max(nums[0],nums[1]);
+        // vector<int> a(nums.size(),0);
+        // a[0]=nums[0];
+        // a[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<nums.size();i++)
+        //     a[i] = max(a[i-2]+nums[i],a[i-1]);
+        
+        
+        
+        
+        //Time O(N)  space O(1)
         if(nums.size()==1)
             return nums[0];
         if(nums.size()==2)
             return max(nums[0],nums[1]);
-        vector<int> a(nums.size(),0);
-        a[0]=nums[0];
-        a[1]=max(nums[0],nums[1]);
-        for(int i=2;i<nums.size();i++)
-            a[i] = max(a[i-2]+nums[i],a[i-1]);
+        int pres;
+        int prev2=nums[0];
+        int prev1=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            pres = max(prev2+nums[i],prev1);
+            prev2=prev1;
+            prev1=pres;
+        }
         
-        
-        for(int i=0;i<nums.size();i++)
-            cout<<a[i]<<" ";
-
-        
-        return a[nums.size()-1];
+        return pres;
     }      
 };
