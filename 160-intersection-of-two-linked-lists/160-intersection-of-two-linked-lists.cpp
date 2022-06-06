6dfbeec6542d25b439f6050549c6ab8dbcb06b88
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-06 20:00:28 +0530

    Time: 83 ms (26.29%), Space: 14.6 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,18 +8,20 @@
  */
 class Solution {
 public:
+    
+    //intution - calcu the differnce between the len of 2 linkend list and place the ptr of the longer list by the differnce so that once they traverse they meet at intersection.
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* pres = headA;
-        while(pres){
-            ListNode* prev = headB;
-            while(prev){
-                if(pres==prev)
-                    return pres;
-                prev=prev->next;
-            }
-            pres=pres->next;
+        ListNode* a = headA;
+        ListNode* b = headB;
+        if(a==NULL || b==NULL) return NULL;
+        while(a!=b){
+           if(a==NULL) a = headB;
+            else a = a->next;
+            
+            if(b==NULL) b = headA;
+            else b = b->next;
+            
         }
-        
-        return NULL;
+        return a;
     }
 };
