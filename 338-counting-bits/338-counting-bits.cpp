5ae66fa37245e5ac9d8be968fc82a19bc2e0a032
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-07 21:32:36 +0530

    Time: 9 ms (45.30%), Space: 7.8 MB (74.14%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,20 +3,11 @@ public:
     vector<int> countBits(int n) {
         vector<int> temp(n+1,0);
         for(int i=1;i<=n;i++){
-            if((i-1)&1 == 0)
-                temp[i] = temp[i-1]+1;
+            if(i%2)
+                temp[i]=temp[i-1]+1;
+            else
+                temp[i] = temp[i/2];
             
-            else{
-                int n = i;
-                int count = 0;
-                while(n){
-                    if(n & 1) 
-                        count+=1;
-                    n=n>>1;
-                }
-                temp[i]=count;
-                
-            }
                 
         }
         
