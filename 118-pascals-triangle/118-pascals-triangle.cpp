8f6564ac1bce7ae9885d4129af081d7ca1d04c9c
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-07 22:03:07 +0530

    Time: 5 ms (12.79%), Space: 6.5 MB (33.19%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,16 +3,8 @@ public:
     vector<vector<int>> generate(int numRows) {
         
         vector<vector<int>> temp;
-        vector<int> a = {1};
-        vector<int> b ={1,1};
-        temp.push_back(a);
-        if(numRows==1)
-            return temp;
-        temp.push_back(b);
-        if(numRows==2)
-            return temp;
-        
-        for(int i=2;i<numRows;i++){
+    
+        for(int i=0;i<numRows;i++){
             vector<int> dummy(i+1,1);
             for(int j=0;j<i;j++){
                 if(j!=0)
