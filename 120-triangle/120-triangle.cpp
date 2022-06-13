0cf99d2a673b9d6fcdc06ef5df8434b2fbc98bcb
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-14 05:10:26 +0530

    Time: 10 ms (44.32%), Space: 8.8 MB (55.52%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -7,18 +7,33 @@ public:
     //     return tri[row][col] + min(cal(tri,row+1,col),cal(tri,row+1,col+1));
     // }
     
+    int cal2(vector<vector<int>> tri){
+        int n = tri.size();
+        vector<int> a(n,0);
+        for(int i=0;i<n;i++)
+            a[i] = tri[n-1][i];
+        
+        for(int i = n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                a[j] = tri[i][j] + min(a[j],a[j+1]);
+            }
+        }
+        return a[0];
+    }
     
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dummy(n,vector<int>(n,0));
-        for(int j=0;j<n;j++)
-            dummy[n-1][j] = triangle[n-1][j];
-        for(int i=n-2;i>=0;i--){
-                for(int j = i;j>=0;j--){
-                    dummy[i][j] = triangle[i][j] + min(dummy[i+1][j],dummy[i+1][j+1]);
-                }
-        }
-        return dummy[0][0];
+        // int n = triangle.size();
+        // vector<vector<int>> dummy(n,vector<int>(n,0));
+        // for(int j=0;j<n;j++)
+        //     dummy[n-1][j] = triangle[n-1][j];
+        // for(int i=n-2;i>=0;i--){
+        //         for(int j = i;j>=0;j--){
+        //             dummy[i][j] = triangle[i][j] + min(dummy[i+1][j],dummy[i+1][j+1]);
+        //         }
+        // }
+        // return dummy[0][0];
+        
+        return cal2(triangle);
     }
 };
