d8410d6541c8cf58ea00eb2cce4091bbdd840435
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-06-30 23:39:09 +0530

    Time: 357 ms (5.09%), Space: 17.9 MB (8.82%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
+    //time - O(N) 
     int minDeletions(string s) {
+        
         int maxi = INT_MIN,count = 0;
+        //cal the freq 
         map<char,int> m;
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
             if(m[s[i]]>maxi)
                 maxi = m[s[i]];
         }
-        
+
         vector<int> a(maxi+1,0);
-        
+        //traversing the map and finding immediate empty places when clashed b/w freq occurs;
         for(auto it=m.begin();it!=m.end();it++){
-            // cout<<it->second<<" "<<a[it->second]<<endl;
             if(a[it->second]){
                 int l = it->second;
                 while(l && a[l]){
                     count++;
                     l--;
                 }   
-                if(l)
-                    a[l]=1;
+                if(l) a[l]=1;
             }else
                 a[it->second]=1;
         }
-        
-        for(int i=0;i<a.size();i++)
-            cout<<a[i]<<" ";
+
         return count;
         
     }
