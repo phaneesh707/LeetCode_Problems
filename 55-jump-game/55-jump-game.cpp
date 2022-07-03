f72d379b1143adaf285669a449cc64e83eec0488
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-07-03 14:07:21 +0530

    Time: 120 ms (38.22%), Space: 50.5 MB (10.06%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,39 +1,70 @@
 class Solution {
 public:
-    // recursion
-    // void jump(vector<int> nums,int index,int& flag){
-    //      if(index >= (nums.size()-1)){
-    //          flag = 1;
-    //          return;
-    //      }   
-    //     for(int i = 1;i<=nums[index];i++)
-    //         jump(nums,index+i,flag);
-    //     return;
-    // }
+    int recur(int n,int idx,vector<int> a){
+        if(idx== n)
+            return 1;
+        int res=0;
+        for(int i=1;i<=a[idx];i++){
+            res = recur(n,idx+i,a);
+            if(res) break;
+        }
+        return res;
+    }
     
-    bool canJump(vector<int>& nums) {
-       //  int flag = false;
-       //  jump(nums,0,flag);
-       // return flag;
-        // if(!nums[0]) return 0;
-        int n = nums.size();
-        vector<int> dummy(n,0);
-        dummy[0] = 1;
+    
+    int memo(int n,int idx,vector<int> a,vector<int>& dp){
+        if(idx== n)
+            return 1;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        
+        int res=0;
+        for(int i=1;i<=a[idx];i++){
+            res = recur(n,idx+i,a);
+            if(res) break;
+        }
+        return dp[idx] =  res;
+    }
+    
+    
+    int tabu(vector<int> a){
+        int n = a.size();
+        vector<int> dp(n,0);
+        dp[0] = 1;
+        
         for(int i=0;i<n-1;i++){
-            if(dummy[i]){
-                if(dummy[n-1] || i+nums[i]>=n-1){
-                    dummy[n-1] = 1;
-                    break;
+            if(dp[i]==1){
+                for(int j=1;j<=a[i];j++){
+                    if(i+j<n)
+                        dp[i+j]=1;
                 }
-            
-                for(int j=1;j<=nums[i];j++)
-                    dummy[i+j] = 1;
             }
         }
         
-        // for(int i=0;i<n;i++)
-        //     cout<<dummy[i]<<" ";
+        return dp[n-1];
+    }
+    
+    
+    //very optimized code
+    bool opt(vector<int> a){
+        int n = a.size();
+        int minJump = 0;
+        for(int i=n-2;i>=0;i--){
+            minJump++;
+            if(a[i]>=minJump)
+                minJump = 0;
+            
+        }
         
-        return dummy[n-1];
+        return !minJump;
+    }
+    
+    bool canJump(vector<int>& nums) {
+     
+        // return recur(nums.size()-1,0,nums);
+        // vector<int> dp(nums.size(),-1);
+        // return memo(nums.size()-1,0,nums,dp);
+        // return tabu(nums);
+        return opt(nums);
     }
 };
