d81052b897a7b3f1315cef4fd61b4e4db870395e
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-07-11 23:13:30 +0530

    Time: 10 ms (51.48%), Space: 9 MB (36.46%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,39 +1,73 @@
 class Solution {
 public:
-    //recursive - TLE
-    // int cal(vector<vector<int>> tri,int row,int col){
-    //     if(row==tri.size())
-    //         return 0;
-    //     return tri[row][col] + min(cal(tri,row+1,col),cal(tri,row+1,col+1));
-    // }
     
-    int cal2(vector<vector<int>> tri){
-        int n = tri.size();
-        vector<int> a(n,0);
-        for(int i=0;i<n;i++)
-            a[i] = tri[n-1][i];
+    int recur(vector<vector<int>> a,int i,int j){
+        if(i==a.size()-1)
+            return a[i][j];
+    
+        int down = a[i][j] + recur(a,i+1,j);
+        int diag = a[i][j] + recur(a,i+1,j+1);
+        return min(down,diag);
+    }
+    
+    int memo(vector<vector<int>> a,int i,int j,vector<vector<int>> dp){
+        if(i==a.size()-1)
+            return a[i][j];
         
-        for(int i = n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                a[j] = tri[i][j] + min(a[j],a[j+1]);
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int down = a[i][j] + recur(a,i+1,j);
+        int diag = a[i][j] + recur(a,i+1,j+1);
+        return dp[i][j] = min(down,diag);
+    }
+    
+    int tabu(vector<vector<int>> a){
+        int n = a.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++)
+            dp[n-1][i] = a[n-1][i];
+        
+        //tabu always reverse of recur
+        for(int i=n-2;i>=0;i--){
+            for(int j = i;j>=0;j--){
+                dp[i][j] = a[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
             }
         }
-        return a[0];
+        
+        return dp[0][0];
+            
     }
     
     
-    int minimumTotal(vector<vector<int>>& triangle) {
-        // int n = triangle.size();
-        // vector<vector<int>> dummy(n,vector<int>(n,0));
-        // for(int j=0;j<n;j++)
-        //     dummy[n-1][j] = triangle[n-1][j];
-        // for(int i=n-2;i>=0;i--){
-        //         for(int j = i;j>=0;j--){
-        //             dummy[i][j] = triangle[i][j] + min(dummy[i+1][j],dummy[i+1][j+1]);
-        //         }
-        // }
-        // return dummy[0][0];
+    int spaceOpt(vector<vector<int>> a){
+        int n = a.size();
+        vector<int> prev(n,-1);
+        vector<int> curr(n,-1);
+        for(int i=0;i<n;i++)
+            prev[i] = a[n-1][i];
         
-        return cal2(triangle);
+        for(int i=n-2;i>=0;i--){
+            for(int j = i;j>=0;j--)
+                curr[j] = a[i][j] + min(prev[j],prev[j+1]);
+            
+            prev = curr;
+            
+        }
+        
+        return prev[0];
+            
+    }
+    
+    int minimumTotal(vector<vector<int>>& t) {
+        // return recur(t,0,0);
+        
+        // vector<vector<int>> dp(t.size(),vector<int>(t.size(),-1));
+        // return memo(t,0,0,dp);
+        
+        // return tabu(t);
+        
+        return spaceOpt(t);
+        
     }
 };
