c388e3ce5bbc96e6dec42284a6ae0a536643f55c
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-08-08 16:45:35 +0530

    Time: 63 ms (47.67%), Space: 30.2 MB (7.03%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,99 @@
+// class Solution {
+// public:
+    
+//     int recur(string s, string t,int idx1,int idx2){
+//         if(idx2>=0 && idx1<0)
+//             return 0;
+//         if(idx2<0 && idx1>=-1)
+//             return 1;
+        
+//         int take = 0,notTake = 0;
+//         if(s[idx1] == t[idx2])
+//             take = recur(s,t,idx1-1,idx2-1);
+//         else 
+//             notTake = recur(s,t,idx1-1,idx2);
+//         return take + notTake;
+            
+//     }
+    
+    
+//     int tabu(string s, string t){
+//         int n = s.size(),m = t.size();
+//         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+//         for(int i=0;i<=n;i++)
+//             dp[i][0] = 1;
+        
+        
+//         for(int idx1=1;idx1<=n;idx1++){
+//             for(int idx2=1;idx2<=m;idx2++){
+                
+//                 int take = 0,notTake = 0;
+//                 if(s[idx1-1] == t[idx2-1])
+//                     take = dp[idx1-1][idx2-1];
+//                 else 
+//                     notTake = dp[idx1-1][idx2];
+//                 dp[idx1][idx2] = take + notTake;
+//             }
+//         }
+    
+        
+//         return dp[n][m];
+//     }
+    
+    
+//     int numDistinct(string s, string t) {
+//         int n = s.size(),m = t.size();
+//         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+//         // return recur(s,t,n-1,m-1);
+//         // return memo(s,t,n-1,m-1,dp);
+//         return tabu(s,t);
+//     }
+// };
+
+
+
+
+class Solution {
+public:
+    
+    int recur(string s, string t,int idx1,int idx2){
+        if(idx2<0) return 1;
+        if(idx1<0) return 0;
+        
+        
+        if(s[idx1] == t[idx2])
+            return recur(s,t,idx1-1,idx2-1) + recur(s,t,idx1-1,idx2);
+        else
+            return recur(s,t,idx1-1,idx2);
+    }
+    
+    int tabu(string s, string t){
+        int n = s.size(),m = t.size();
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        
+        
+        for(int idx1=1;idx1<=n;idx1++){
+            for(int idx2=1;idx2<=m;idx2++){
+                
+                if(s[idx1-1] == t[idx2-1])
+                    dp[idx1][idx2] =  dp[idx1-1][idx2-1] + dp[idx1-1][idx2];
+                else
+                    dp[idx1][idx2] =  dp[idx1-1][idx2];
+            }
+        }
+    
+        
+        return (int)dp[n][m];
+    }
+    
+    
+    int numDistinct(string s, string t) {
+        int n = s.size(),m = t.size();
+        // return recur(s,t,n-1,m-1);
+        return tabu(s,t);
+    }
+};
+
+
