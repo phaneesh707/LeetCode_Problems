26f5ce4967a1cd465309c10fdeb82fdd3ff008df
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-09-05 18:28:11 +0530

    Time: 80 ms (18.28%), Space: 13.2 MB (28.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -9,7 +9,7 @@ public:
     }
     
     void dfs(vector<vector<char>>& grid,vector<vector<int>>& visited,int i,int j){
-        cout<<"recur "<<i<<" "<<j<<endl;
+        
         int m = size(grid),n = size(grid[0]);
         visited[i][j] = 1;
         if(isValid(i-1,j,m,n) && grid[i-1][j]=='1' && !visited[i-1][j])
@@ -23,16 +23,28 @@ public:
         
     }
     
+    vector<int> d = {0,1,0,-1,0};
+    void cleanDFS(vector<vector<char>>& a,vector<vector<int>>& visited,int i,int j){     
+        visited[i][j] = 1;
+        for(int k=0;k<4;k++){
+            int x = i+d[k];
+            int y = j+d[k+1];
+            if(isValid(x,y,size(a),size(a[0])) && !visited[x][y] && a[x][y]=='1')
+                cleanDFS(a,visited,x,y);
+        }
+        
+    }
    
     int numIslands(vector<vector<char>>& grid){
         int n = size(grid),m = size(grid[0]),count = 0;
         vector<vector<int>> visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                cout<<i<<" "<<j<<endl;
                 if(grid[i][j]=='1' && !visited[i][j]){
                     count++;
-                    dfs(grid,visited,i,j);
+                    // cout<<i<<" "<<j<<endl;
+                    cleanDFS(grid,visited,i,j);
+                    // dfs(grid,visited,i,j);
                 }     
             }
         }
