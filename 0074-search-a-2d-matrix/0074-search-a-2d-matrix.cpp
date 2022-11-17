b23943dcf7f0bf0c586c6b3aa44876ce7b138131
Phaneesh <84644157+phaneesh707@users.noreply.github.com>
2022-11-18 01:41:27 +0530

    Time: 7 ms (70.12%), Space: 9.6 MB (51.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    //o(M*log(N))
     bool binarySrc(vector<int>& a,int k){
         int l=0,h = size(a)-1;
         while(l<=h){
@@ -15,13 +15,41 @@ public:
         return false;
     }
     
+    // o(log(m)*log(n))
+    pair<int,int> binarySrc1(vector<int>& a,int k){
+        int l=0,h = size(a)-1;
+        while(h-l>1){
+            int mid = (l+h)/2;
+            if(a[mid]<k)
+                l = mid;
+            else 
+                h = mid;
+            
+        }
+        return {l,h};
+    }
+    
     
     bool searchMatrix(vector<vector<int>>& a, int target) {
         int r = size(a),c = size(a[0]);
-        for(int i=0;i<r;i++){
-            if(binarySrc(a[i],target))
-                return true;
-        }
+        // for(int i=0;i<r;i++){
+        //     if(binarySrc(a[i],target))
+        //         return true;
+        // }
+        // return false;
+        
+        vector<int> b(r);
+        
+        for(int i=0;i<r;i++)
+            b[i] = a[i][0];
+            
+        pair<int,int> res = binarySrc1(b,target);
+        cout<<res.first<<res.second<<endl;
+        if(binarySrc(a[res.first],target) || binarySrc(a[res.second],target))
+            return true;
         return false;
+        
+        
+        
     }
 };
